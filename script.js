bd7511a5e0fb6778fef03d5223b976f779dab70d
Andacanaver <moe.jacobw@gmail.com>
2019-08-19 11:43:30 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+$(function() {
+  
+    var pet_info = {
+        "name" : "Mightypup",
+        "weight" : 7,
+        "happiness" : 0
+    }
+    
+    checkAndUpdatePetInfoInHtml();
+    
+    $('.treat-button').click(clickedTreatButton);
+    $('.play-button').click(clickedPlayButton);
+    $('.exercise-button').click(clickedExerciseButton);
+    
+    function clickedTreatButton() {
+        pet_info['happiness'] = pet_info['happiness'] + 1;
+        pet_info['weight'] = pet_info['weight'] + 2;
+        checkAndUpdatePetInfoInHtml();
+    }
+    function clickedPlayButton() {
+        pet_info['happiness'] = pet_info['happiness'] + 2;
+        pet_info['weight'] = pet_info['weight'] - 1;
+        checkAndUpdatePetInfoInHtml();
+    }
+    function clickedExerciseButton() {
+        pet_info['happiness'] = pet_info['happiness'] - 1;
+        pet_info['weight'] = pet_info['weight'] - 1;
+        checkAndUpdatePetInfoInHtml();
+    }
+    function checkAndUpdatePetInfoInHtml() {
+        checkWeightAndHappinessBeforeUpdating();
+        updatePetInfoInHtml();
+    }
+    function checkWeightAndHappinessBeforeUpdating() {
+        if (pet_info['weight'] < 1) {
+            pet_info['weight'] = 1;
+        }
+    }
+    function updatePetInfoInHtml() {
+        $('.name').text(pet_info['name']);
+        $('.weight').text(pet_info['weight']);
+        $('.happiness').text(pet_info['happiness']);
+    }
+})
